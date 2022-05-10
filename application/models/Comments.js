9ae64560b51c7cf4986e068f5d019bb9ab52dc34
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2022-05-10 11:34:10 -0700

    Added Comments.js

---
+++ application/models/Comments.js
@@ -0,0 +1,34 @@
+var db = require('../config/database');
+const CommentModel = {};
+
+CommentModel.create = (userId, postId, comment) => {
+    let baseSQL = `INSERT INTO comments(comment,fk_postId,fk_authorId) VALUES (?,?,?);`;
+    return db.query(baseSQL, [comment, postId, userId])
+        .then(([results, fields]) => {
+            if (results && results.affectedRows) {
+                return Promise.resolve(results.insertId);
+            } else {
+                return Promise.resolve(-1);
+            }
+        })
+        .catch((err) => Promise.reject(err));
+};
+
+CommentModel.getCommentsForPost = (postId) => {
+    let baseSQL = `SELECT u.username,c.comment,c.created,c.id 
+     FROM comments c
+     JOIN user u 
+     on u.id=fk_authorId 
+     WHERE c.fk_postId=? 
+     ORDER BY c.created DESC`;
+
+    return db.query(baseSQL, [postId])
+        .then(([results, fields]) => {
+            return Promise.resolve(results);
+
+        })
+        .catch((err) => Promise.reject(err));
+};
+
+
+module.exports = CommentModel;
